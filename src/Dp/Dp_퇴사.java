8c072e714f4bb4ecdf137d53d8710d95c4f60b7d
SeokJun <wnstjr120422@naver.com>
2022-07-04 18:44:57 +0900

    백준 알고리즘 연습 ver.2 / Dp_긴증가부분수열2는 미완

--- src/Dp/Dp_퇴사.java
+++ src/Dp/Dp_퇴사.java
@@ -5,8 +5,25 @@ public class Dp_퇴사 {
     public void run(){
         Scanner sc=new Scanner(System.in);
         int day=sc.nextInt();
-        int[][] dp=new int[5][1000];
+        int[] workday=new int[day+2];
+        int[] salary=new int[1000];
+        int[] dp=new int[1000]; //최대 급여를 저장하는 배열
+        int maxnum=0;
 
+        for(int i=0;i<day;i++){
+            workday[i]=sc.nextInt();
+            salary[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<=day;i++){
+            dp[i]=Math.max(maxnum,dp[i]);
+            dp[workday[i]+i]=Math.max(dp[workday[i]+i],dp[i]+salary[i]);
+            maxnum=Math.max(dp[i],maxnum);
+        }
+
+        System.out.println(maxnum);
+        sc.close();
+        return;
 
     }
 
