d9b3549370ba5dc53f35dfaef221fbcfe8e77108
SeokJun <wnstjr120422@naver.com>
2022-07-06 19:23:50 +0900

    백준 알고리즘 연습 ver.2 / Dfs알고리즘

---
+++ src/DFS/DFS_바이러스.java
@@ -0,0 +1,13 @@
+package DFS;
+import java.util.*;
+public class DFS_바이러스 {
+    public void run(){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+    }
+
+    public static void main(String[] args){
+        DFS_바이러스 dfs_바이러스=new DFS_바이러스();
+        dfs_바이러스.run();
+    }
+}
