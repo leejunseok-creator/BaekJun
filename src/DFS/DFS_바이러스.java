94000c02952f18fc885906cb8ed923bd1ec395c0
SeokJun <wnstjr120422@naver.com>
2022-07-07 22:24:52 +0900

    백준 알고리즘 연습 ver.2 / Dfs알고리즘

--- src/DFS/DFS_바이러스.java
+++ src/DFS/DFS_바이러스.java
@@ -1,9 +1,41 @@
 package DFS;
 import java.util.*;
 public class DFS_바이러스 {
+    int count=0;
+    int computer;
+    int couple;
+    int[][] map;
+    int[] visit;
     public void run(){
         Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
+        computer=sc.nextInt();
+        couple=sc.nextInt();
+        map=new int[computer+1][computer+1];
+        visit=new int[computer+1];
+
+        for(int i=0;i<couple;i++){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            map[x][y]=1;
+            map[y][x]=1;
+        }
+
+        dfs(1);
+        System.out.println(count);
+        sc.close();
+        return;
+    }
+
+    public void dfs(int index){
+        visit[index]=1;
+
+        for(int i=1;i<=computer;i++){
+            if(map[index][i]==1 && visit[i]==0){
+                    visit[i]=1;
+                    count++;
+                    dfs(i);
+            }
+        }
     }
 
     public static void main(String[] args){
